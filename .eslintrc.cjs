f2e0f7f668f518baa9c429a67e7f41f173728615
Finn Hsu <finn.hsu@ecloudvalley.com>
2024-05-11 16:53:35 +0800

    feat: dev setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,19 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended"
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest"
+  },
+  "rules": {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "indent": ["error", 2],
+    "comma-dangle": ["error", "never"],
+    "object-curly-spacing": ["error", "always"]
+  },
+  ignorePatterns: [".eslintrc.js", "node_modules", "build", "dist", "public"]
+};
