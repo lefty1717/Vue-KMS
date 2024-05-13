eaf0acceda49c751116acc37941569ccbe2bdb59
Finn Hsu <finn.hsu@ecloudvalley.com>
2024-05-13 16:39:42 +0800

    feat: primevue and primevue icon

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+// primevue
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/aura-light-green/theme.css";
+
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,5 +14,10 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.use(PrimeVue, {
+  ripple: true,
+  inputStyle: "outlined"
+});
 
 app.mount("#app");
